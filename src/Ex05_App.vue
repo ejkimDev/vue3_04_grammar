e58cab898ce77e91574f90e7e714a77e929a2549
ejkimDev <ejkim.dev@gmail.com>
2022-03-02 04:55:28 +0900

    05 클래스와 스타일 바인딩

---
+++ src/Ex05_App.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- 2개의 객체 데이터를 style로 연결하려면 배열 구문 사용 -->
+  <h1
+    :style="[fontStyle, backgroundStyle]"
+    @click="changeStyle">
+    Hello?!
+    </h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fontStyle: {
+        color: 'orange',
+        fontSize: '30px'
+      },
+      backgroundStyle: {
+        backgroundColor: 'black'
+      }
+    }
+  },
+  methods: {
+    changeStyle() {
+      this.fontStyle.color = 'red';
+      this.fontStyle.fontSize = '50px';
+    }
+  }
+}
+</script>
