2fb2e51ca53b4ece7addc5400941a777e71348a0
ejkimDev <ejkim.dev@gmail.com>
2022-04-09 03:35:54 +0900

    10 zl 수식어

---
+++ src/Ex10_App.vue
@@ -0,0 +1,17 @@
+<template>
+  <input 
+    type="text" 
+    @keydown.ctrl.shift.a="handler" />  <!-- ctrl 키 + shift 키 + a 누르면 handler 메서드 실행됨 -->
+</template>
+
+<script>
+export default {
+  methods: {
+    handler(event) {
+      console.log('Enter!');
+    }
+  }
+}
+</script>
+
+
