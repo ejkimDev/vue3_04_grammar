af63ed456ffe544a97ca99bef2775bc185416a0f
ejkimDev <ejkim.dev@gmail.com>
2022-04-09 04:18:12 +0900

    11 폼 입력 바인딩

---
+++ src/Ex11_App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <input 
+    type="text"
+    v-model="msg" />
+  <h1>{{ checked }}</h1>
+  <input type="checkbox" v-model="checked"> 
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello world!',
+      checked: false
+    }
+  }
+}
+</script>
