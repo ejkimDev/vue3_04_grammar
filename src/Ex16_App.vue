13b441da161e5711d260212c3911672f66d70d65
ejkimDev <ejkim.dev@gmail.com>
2022-04-19 05:22:28 +0900

    16 컴포넌트 Slot

---
+++ src/Ex16_App.vue
@@ -0,0 +1,16 @@
+<template>
+  <MyBtn>
+    <template v-slot:text><span>Banana</span> </template>
+    <template #icon><span>(B)</span> </template>
+  </MyBtn>
+</template>
+
+<script>
+import MyBtn from '~/components/Ex16_MyBtn'
+
+export default {
+  components: {
+    MyBtn
+  }
+}
+</script>
---
+++ src/components/Ex16_MyBtn.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="btn"> 
+    <slot name="icon"></slot>   
+    <slot name="text"></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+ .btn {
+   display: inline-block;
+   margin: 4px;
+   padding: 6px 12px;
+   border-radius: 4px;
+   background-color: gray;
+   color: white;
+   cursor: pointer;
+   &.large {
+   font-size: 20px;
+   padding: 10px 20px;
+  }
+ }
+</style>
