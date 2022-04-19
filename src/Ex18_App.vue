fd64a6b9001068bc58fcd32654ec322293a006ca
ejkimDev <ejkim.dev@gmail.com>
2022-04-19 21:48:14 +0900

    18 컴포넌트 - Refs

---
+++ src/Ex18_App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Hello ref="hello" /> 
+</template>
+
+<script>
+import Hello from '~/components/Ex18_Hello'
+
+export default {
+  components : {
+    Hello
+  },
+  mounted() {   // 컴포넌트가 HTML 구조와 연결된 직후
+    console.log(this.$refs.hello.$refs.good);
+  }
+
+}
+</script>
